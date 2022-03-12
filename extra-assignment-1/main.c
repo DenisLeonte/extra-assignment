e95183b524aa080f86b30ab5d421ab33ac6662b5
MyBoyDoru <denis.leonte123@gmail.com>
2022-03-12 15:10:28 +0200

    extra-assignment-1 begin

---
+++ extra-assignment-1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//This program will be tasked with cypering and decyphering the Caesar chyper.
+//The Caesar cypher takes a phrase and off-sets each letter by a key K.
+//We will de-cypher a cyphered phrase using the ci-squared algorithm. We create a frequency map for each letter and we compare with the english frequency map.
+//The lowest ci-square will be the key offset.
+//This program will also provide an UI and menu for more actions.
+
+int main()
+{
+    return 0;
+}
